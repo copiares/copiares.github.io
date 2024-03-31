5bf4714c71a447aeef6507a1b2c76aeb8e3dbbea
copiares <163731928+copiares@users.noreply.github.com>
2024-03-31 16:48:16 +0100

    script.js

--- js/script.js
+++ js/script.js
@@ -3,28 +3,24 @@
 
 // script funcoes 
 
-    //Gerador QRCode
-        async function genQR() {
+// Gerador QRCode
+async function genQR() {
     var gapi = "https://chart.googleapis.com/chart?chf=bg,s,65432100&cht=qr&chs=";
     var myimg = document.getElementById("img");
     var mytext = document.getElementById("qrtext").value;
     var mysize = document.getElementById("size").value;
 
-    if (mytext !== "" && mysize == "100") {
-        myimg.src = gapi + "100x100" + "&chl=" + mytext;
-        // https://chart.googleapis.com/chart?cht=qr&chs=100x100&chl=hello
-    } else if (mytext !== "" && mysize == "150") {
-        myimg.src = gapi + "150x150" + "&chl=" + mytext;
-    } else if (mytext !== "" && mysize == "200") {
-        myimg.src = gapi + "200x200" + "&chl=" + mytext;
-    } else if (mytext !== "" && mysize == "250") {
-        myimg.src = gapi + "250x250" + "&chl=" + mytext;
-    } else if (mytext !== "" && mysize == "300") {
-        myimg.src = gapi + "300x300" + "&chl=" + mytext;
+    if (mytext !== "") {
+        if (mysize >= 100 && mysize <= 300 && mysize % 50 === 0) {
+            myimg.src = gapi + mysize + "x" + mysize + "&chl=" + mytext;
+        } else {
+            alert("Invalid size. Size should be a multiple of 50 between 100 and 300.");
+        }
     } else {
         alert("Please Enter Text");
     }
 }
+
 
    
     //Gerador de Curriculo em PDF
